fc2c5651615c7c6eca39828df5ec2512cce9a472
Masorm <100272527+Masorm@users.noreply.github.com>
2022-06-28 23:35:34 +0900

    add: Package更新の拡張処理追加 (#3)
    Co-authored-by: Masorm <fw_yama9@yahoo.co.jp>

--- Assets/Plugins/BaseView/Editor/EditorExtensions.cs
+++ Assets/Plugins/BaseView/Editor/EditorExtensions.cs
@@ -1,10 +1,15 @@
 using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 namespace BaseView.Plugins.Editor
 {
     public class EditorExtensions : EditorWindow
     {
+        private static AddRequest _addRequest;
+        private const string PACKAGE_PATH = "https://github.com/Masorm/BaseView.git?path=Assets/Plugins/BaseView";
+        
         [MenuItem("Extension/ShowWindow")]
         private static void ShowWindow()
         {
@@ -17,6 +22,29 @@ namespace BaseView.Plugins.Editor
             if (GUILayout.Button("Button"))
             {
                 Debug.Log("Pushed!!");
+            }
+
+            if (GUILayout.Button("Update Package"))
+            {
+                _addRequest = Client.Add(PACKAGE_PATH);
+                EditorApplication.update += Progress;
+            }
+        }
+
+        private static void Progress()
+        {
+            if (_addRequest.IsCompleted)
+            {
+                if (_addRequest.Status == StatusCode.Success)
+                {
+                    Debug.Log($"Install完了: {_addRequest.Result.version}");
+                }
+                else if (_addRequest.Status >= StatusCode.Failure)
+                {
+                    Debug.LogError(_addRequest.Error.message);
+                }
+
+                EditorApplication.update -= Progress;
             }
         }
     }
