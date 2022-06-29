eed1ecb31c362143c5379c5b2284638eb3734dc3
Masorm <fw_yama9@yahoo.co.jp>
2022-06-29 11:25:05 +0900

    update: Addressableの初期設定等を自動で行う処理追加

--- Assets/Plugins/BaseView/Editor/EditorExtensions.cs
+++ Assets/Plugins/BaseView/Editor/EditorExtensions.cs
@@ -1,7 +1,12 @@
+using System.Linq;
 using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.AddressableAssets.Settings.GroupSchemas;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace BaseView.Plugins.Editor
 {
@@ -19,9 +24,9 @@ namespace BaseView.Plugins.Editor
 
         private void OnGUI()
         {
-            if (GUILayout.Button("Button"))
+            if (GUILayout.Button("Set&Build Addressable"))
             {
-                Debug.Log("Pushed!!");
+                SetAddressable();
             }
 
             if (GUILayout.Button("Update Package"))
@@ -46,6 +51,81 @@ namespace BaseView.Plugins.Editor
 
                 EditorApplication.update -= Progress;
             }
+        }
+
+        private static AddressableAssetSettings settings;
+        private static AddressableAssetGroup activeSceneGroup;
+        private const string DEFAULT_LOCAL_GROUP_NAME = "Default Local Group";
+        private static string ACTIVE_SCENE_NAME => SceneManager.GetActiveScene().name;
+
+        private static void SetAddressable()
+        {
+            if (!AddressableAssetSettingsDefaultObject.SettingsExists)
+            {
+                // AddressableAssetSettingsDefaultObjectがなければ作成する
+                settings = AddressableAssetSettingsDefaultObject.GetSettings(true);
+                settings.BuildRemoteCatalog = true;
+                settings.OverridePlayerVersion = "v0.0.1";
+            }
+            else
+            {
+                // // AddressableAssetSettingsDefaultObjectがあれば既存のものを使う
+                settings = AddressableAssetSettingsDefaultObject.Settings;
+            }
+
+            activeSceneGroup = settings.groups.FirstOrDefault(x => x.Name == ACTIVE_SCENE_NAME);
+            if (activeSceneGroup == null)
+            {
+                var groupTemplate = settings.GetGroupTemplateObject(0) as AddressableAssetGroupTemplate;
+                activeSceneGroup = settings.CreateGroup(ACTIVE_SCENE_NAME, true, false, false, null, groupTemplate.GetTypes());
+            }
+
+            SetProfileSettings(settings);
+
+            SetSchemaSettings(activeSceneGroup, settings);
+        
+            DeleteDefaultGroup(settings);
+            AssetDatabase.SaveAssets();
+        }
+
+        private const string BUILD_PATH_VARIABLE_NAME = "CustomBuildPath";
+        private const string LOAD_PATH_VARIABLE_NAME = "CustomLoadPath";
+        private const string BUILD_PATH_DEFAULT_VALUE = "ServerData/WebGL";
+        private const string LOAD_PATH_DEFAULT_VALUE = "https://qiita.com";
+    
+        /// <summary>
+        /// Set ProfileSettings
+        /// </summary>
+        /// <param name="settings"></param>
+        private static void SetProfileSettings(AddressableAssetSettings settings)
+        {
+            settings.profileSettings.CreateValue(BUILD_PATH_VARIABLE_NAME, BUILD_PATH_DEFAULT_VALUE);
+            settings.profileSettings.CreateValue(LOAD_PATH_VARIABLE_NAME, LOAD_PATH_DEFAULT_VALUE);
+            settings.RemoteCatalogBuildPath.SetVariableByName(settings, BUILD_PATH_VARIABLE_NAME);
+            settings.RemoteCatalogLoadPath.SetVariableByName(settings, LOAD_PATH_VARIABLE_NAME);
+        }
+
+        /// <summary>
+        /// Set SchemaSettings
+        /// </summary>
+        /// <param name="group"></param>
+        private static void SetSchemaSettings(AddressableAssetGroup group, AddressableAssetSettings settings)
+        {
+            var schema = group.GetSchema<BundledAssetGroupSchema>();
+            schema.BundleMode = BundledAssetGroupSchema.BundlePackingMode.PackSeparately;
+            schema.BundleNaming = BundledAssetGroupSchema.BundleNamingStyle.NoHash;
+            schema.BuildPath.SetVariableByName(settings, BUILD_PATH_VARIABLE_NAME);
+            schema.LoadPath.SetVariableByName(settings, LOAD_PATH_VARIABLE_NAME);
+        }
+
+        /// <summary>
+        /// Delete DefaultGroup
+        /// </summary>
+        /// <param name="settings"></param>
+        private static void DeleteDefaultGroup(AddressableAssetSettings settings)
+        {
+            var defaultGroup = settings.groups.FirstOrDefault(x => x.Name == DEFAULT_LOCAL_GROUP_NAME);
+            if (defaultGroup != null) settings.RemoveGroup(defaultGroup);
         }
     }
 }
